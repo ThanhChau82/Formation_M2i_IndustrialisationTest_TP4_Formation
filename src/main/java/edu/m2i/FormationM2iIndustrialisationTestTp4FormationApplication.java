39b24bdd4d5041d944a6948de996fb080e623940
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-03 09:59:29 +0200

    init

---
+++ src/main/java/edu/m2i/FormationM2iIndustrialisationTestTp4FormationApplication.java
@@ -0,0 +1,13 @@
+package edu.m2i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationM2iIndustrialisationTestTp4FormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationM2iIndustrialisationTestTp4FormationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/m2i/FormationM2iIndustrialisationTestTp4FormationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.m2i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationM2iIndustrialisationTestTp4FormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
